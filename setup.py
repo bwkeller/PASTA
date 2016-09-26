4a65c2a4da2ecec73e279562240d3e1597a4fc73
Ben Keller <malzraa@gmail.com>
2016-09-26 13:26:24 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+
+from distutils.core import setup
+
+setup(name='PASTA',
+        version='1.0',
+        description='Python Astronomical Stacking Tool Array',
+        author='Ben Keller',
+        author_email='malzraa@gmail.com',
+        py_modules=['stampy', 'stacky']
+        )
+
